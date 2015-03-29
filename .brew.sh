e3c6a5fc114a0bb210cc27bf7d06e6d61ab7a46f
Juraj Bednar <juraj@bednar.sk>
2015-03-29 18:26:46 +0200

    don't need port knocking server

--- .brew.sh
+++ .brew.sh
@@ -72,7 +72,7 @@ brew install foremost
 brew install hashpump
 brew install hydra
 brew install john
-brew install knock
+#brew install knock
 brew install nmap
 brew install pngcheck
 brew install socat
