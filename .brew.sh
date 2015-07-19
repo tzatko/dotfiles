a40be4e69ff8d660edf90fd2b25d4f34344141d0
Juraj Bednar <juraj@bednar.sk>
2015-07-19 21:46:13 +0200

    drop xpdf, no longer in homebrew

--- .brew.sh
+++ .brew.sh
@@ -78,7 +78,6 @@ brew install tcpflow
 brew install tcpreplay
 brew install tcptrace
 brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
 brew install xz
 
 # Install other useful binaries.
