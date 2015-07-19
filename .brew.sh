35ba53e246ee63ec26409fb1917893de80762480
Juraj Bednar <juraj@bednar.sk>
2015-07-19 22:27:50 +0200

    Some homebrew packages should be installed only on OS X (and not on Linux)

--- .brew.sh
+++ .brew.sh
@@ -3,10 +3,10 @@
 # Install command-line tools using Homebrew.
 
 # Ask for the administrator password upfront.
-sudo -v
+#sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -14,24 +14,43 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade --all
 
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# some packages are only required/wanted on Mac OS X
+if [ $(uname) == "Darwin" ]
+	then
+
+				# Install GNU core utilities (those that come with OS X are outdated).
+				# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+				brew install coreutils
+				sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+				# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+				brew install findutils
+				# Install GNU `sed`, overwriting the built-in `sed`.
+				brew install gnu-sed --with-default-names
+				# Install Bash 4.
+				# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+				# running `chsh`.
+				brew install bash
+				brew tap homebrew/versions
+				brew install bash-completion2
+
+				# Install more recent versions of some OS X tools.
+				brew install vim --override-system-vi
+				brew install homebrew/dupes/grep
+				brew install homebrew/dupes/openssh
+				brew install homebrew/dupes/screen
+				brew install homebrew/php/php55 --with-gmp
+
+				# Install font tools.
+				brew tap bramstein/webfonttools
+				brew install sfnt2woff
+				brew install sfnt2woff-zopfli
+				brew install woff2
+
+				brew install dark-mode
+  fi
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
-
 # mosh
 brew install mobile-shell
 
@@ -43,19 +62,6 @@ brew install wget --with-iri
 # see http://git.io/brew-narwhal-ringo.
 #brew install ringojs
 #brew install narwhal
-
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 brew install aircrack-ng
@@ -82,7 +88,6 @@ brew install xz
 
 # Install other useful binaries.
 brew install ack
-brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-extras
